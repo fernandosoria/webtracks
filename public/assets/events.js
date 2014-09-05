c7fed2c46eb5ed431ff44df072c14bb969027732
fernandosoria <soria.fernando@gmail.com>
2014-09-05 16:54:05 +0000

    Bug fix, moved events.js to public folder

--- public/assets/events.js
+++ public/assets/events.js
@@ -5,7 +5,7 @@ var track = (function(){
 
     var _wt_request = new XMLHttpRequest();
     
-    _wt_request.open("POST", "http://localhost:3000/events.json", true);
+    _wt_request.open("POST", "http://fernandosoria-webtracks.herokuapp.com/events.json", true);
     
     _wt_request.setRequestHeader('Content-Type', 'application/json');
 
